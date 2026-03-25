dd81c4880276365f0d4b2580bbb8c0622fea9a83
SHAIFALI SINGH <shaifalis930@gmail.com>
2026-03-25 15:52:00 +0530

    Add instructions to run the app in Streamlit

---
+++ App.py
@@ -0,0 +1,5 @@
+# Run this file with: streamlit run app.py
+# See Omni_Data_Analyst_Agent.ipynb for full setup
+
+print("Please run the Jupyter notebook in Google Colab")
+print("See README.md for instructions")
